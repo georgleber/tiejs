1f8302be40fa297cd95d1ba3cb21ef577da818ff
Georg Henkel <georg@develman.de>
2015-01-12 19:21:45 +0100

    add user-defined error messages, select all form inputs except buttons

--- dist/tie.js
+++ dist/tie.js
@@ -79,7 +79,7 @@
         };
 
         this.captureFields = function () {
-            $form.find(':input:not(button)').each(function (index, field) {
+            $form.find(':input:not(:button)').each(function (index, field) {
                 var fieldName = $(field).attr('name');
                 if (_findInArray(fieldName, fieldNames) === null) {
                     fieldNames.push({name: fieldName, binding: ""});
@@ -102,11 +102,13 @@
             });
         };
 
-        this.markFieldError = function (fieldNames) {
+        this.markFieldError = function (fieldNames, errorMessage) {
             $.each(fieldNames, function (index, fieldName) {
                 var field = $form.find('[name=' + fieldName + ']');
                 _addFieldError(field);
             });
+
+            _addFormError($form, errorMessage);
         };
 
         function _initForm() {
@@ -682,12 +684,19 @@
             return typeof attr !== 'undefined' && attr !== false;
         }
 
-        function _addFormError(form) {
-            var error = $("<div></div>");
-            error.addClass("alert alert-danger");
-            error.text("Bitte beheben Sie die im Formular hervorgehobenen Fehler");
+        function _addFormError(form, message) {
+            var error = $(".formerror");
+            if(error.length == 0) {
+                error = $("<div></div>");
+                error.addClass("formerror alert alert-danger");
+                form.prepend(error);
+            }
 
-            form.prepend(error);
+            if (message === undefined) {
+                error.text("Bitte beheben Sie die im Formular hervorgehobenen Fehler");
+            } else {
+                error.text(message);
+            }
         }
 
         function _addFieldError(field) {
@@ -739,68 +748,68 @@
         }
 
         function getToolbarTemplate(){
-return '<div class="btn-toolbar" data-role="editor-toolbar" data-target="#editor">' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" title="" data-original-title="Font"><i class="glyphicon glyphicon-font"></i><b class="caret"></b></a>' +
-'<ul class="dropdown-menu">' +
-'<li><a data-edit="fontName Serif" style="font-family:'+"'Serif'" + '>Serif</a></li>' +
-'<li><a data-edit="fontName Sans" style="font-family:'+"'Sans'" + '">Sans</a></li>' +
-'<li><a data-edit="fontName Arial" style="font-family:'+"'Arial'" + '">Arial</a></li>' +
-'<li><a data-edit="fontName Arial Black" style="font-family:'+"'Arial Black'" + '">Arial Black</a></li>' +
-'<li><a data-edit="fontName Courier" style="font-family:'+"'Courier'" + '">Courier</a></li>' +
-'<li><a data-edit="fontName Courier New" style="font-family:'+"'Courier New'" + '">Courier New</a></li>' +
-'<li><a data-edit="fontName Comic Sans MS" style="font-family:'+"'Comic Sans MS'" + '">Comic Sans MS</a></li>' +
-'<li><a data-edit="fontName Helvetica" style="font-family:'+"'Helvetica'" + '">Helvetica</a></li>' +
-'<li><a data-edit="fontName Impact" style="font-family:'+"'Impact'" + '">Impact</a></li>' +
-'<li><a data-edit="fontName Lucida Grande" style="font-family:'+"'Lucida Grande'" + '">Lucida Grande</a></li>' +
-'<li><a data-edit="fontName Lucida Sans" style="font-family:'+"'Lucida Sans'" + '">Lucida Sans</a></li>' +
-'<li><a data-edit="fontName Tahoma" style="font-family:'+"'Tahoma'" + '">Tahoma</a></li>' +
-'<li><a data-edit="fontName Times" style="font-family:'+"'Times'" + '">Times</a></li>' +
-'<li><a data-edit="fontName Times New Roman" style="font-family:'+"'Times New Roman'" + '">Times New Roman</a></li>' +
-'<li><a data-edit="fontName Verdana" style="font-family:'+"'Verdana'" + '">Verdana</a></li></ul> ' +
-'</div>' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" title="" data-original-title="Font Size"><i class="glyphicon glyphicon-text-height"></i>&nbsp;<b class="caret"></b></a>' +
-'<ul class="dropdown-menu">' +
-'<li><a data-edit="fontSize 5"><font size="5">Huge</font></a></li>' +
-'<li><a data-edit="fontSize 3"><font size="3">Normal</font></a></li>' +
-'<li><a data-edit="fontSize 1"><font size="1">Small</font></a></li>' +
-'</ul>' +
-'</div>' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary" data-edit="bold" title="" data-original-title="Bold (Ctrl/Cmd+B)"><i class="glyphicon glyphicon-bold"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="italic" title="" data-original-title="Italic (Ctrl/Cmd+I)"><i class="glyphicon glyphicon-italic"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="underline" title="" data-original-title="Underline (Ctrl/Cmd+U)"><i class="glyphicon glyphicon-text-width"></i></a>' +
-'</div>' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary" data-edit="insertunorderedlist" title="" data-original-title="Bullet list"><i class="glyphicon glyphicon-list"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="insertorderedlist" title="" data-original-title="Number list"><i class="glyphicon glyphicon-list-alt"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="outdent" title="" data-original-title="Reduce indent (Shift+Tab)"><i class="glyphicon glyphicon-indent-left"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="indent" title="" data-original-title="Indent (Tab)"><i class="glyphicon glyphicon-indent-right"></i></a>' +
-'</div>' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary" data-edit="justifyleft" title="" data-original-title="Align Left (Ctrl/Cmd+L)"><i class="glyphicon glyphicon-align-left"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="justifycenter" title="" data-original-title="Center (Ctrl/Cmd+E)"><i class="glyphicon glyphicon-align-center"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="justifyright" title="" data-original-title="Align Right (Ctrl/Cmd+R)"><i class="glyphicon glyphicon-align-right"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="justifyfull" title="" data-original-title="Justify (Ctrl/Cmd+J)"><i class="glyphicon glyphicon-align-justify"></i></a>' +
-'</div>' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" title="" data-original-title="Hyperlink"><i class="glyphicon glyphicon-link"></i></a>' +
-'<div class="dropdown-menu input-append">' +
-'<input class="span2" placeholder="URL" type="text" data-edit="createLink">' +
-'<button class="btn" type="button">Add</button>' +
-'</div>' +
-'<a class="btn btn-xs btn-primary" data-edit="unlink" title="" data-original-title="Remove Hyperlink"><i class="glyphicon glyphicon-remove"></i></a>' +
-'</div>' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary" title="" id="pictureBtn" data-original-title="Insert picture (or just drag &amp; drop)"><i class="glyphicon glyphicon-picture"></i></a>' +
-'<input type="file" data-role="magic-overlay" data-target="#pictureBtn" data-edit="insertImage" style="opacity: 0; position: absolute; top: 0px; left: 0px; width: 37px; height: 30px;">' +
-'</div>' +
-'<div class="btn-group">' +
-'<a class="btn btn-xs btn-primary" data-edit="undo" title="" data-original-title="Undo (Ctrl/Cmd+Z)"><i class="glyphicon glyphicon-backward"></i></a>' +
-'<a class="btn btn-xs btn-primary" data-edit="redo" title="" data-original-title="Redo (Ctrl/Cmd+Y)"><i class="glyphicon glyphicon-forward"></i></a>' +
-'</div>' +
-'</div>';
+            return '<div class="btn-toolbar" data-role="editor-toolbar" data-target="#editor">' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" title="" data-original-title="Font"><i class="glyphicon glyphicon-font"></i><b class="caret"></b></a>' +
+                '<ul class="dropdown-menu">' +
+                '<li><a data-edit="fontName Serif" style="font-family:'+"'Serif'" + '>Serif</a></li>' +
+                '<li><a data-edit="fontName Sans" style="font-family:'+"'Sans'" + '">Sans</a></li>' +
+                '<li><a data-edit="fontName Arial" style="font-family:'+"'Arial'" + '">Arial</a></li>' +
+                '<li><a data-edit="fontName Arial Black" style="font-family:'+"'Arial Black'" + '">Arial Black</a></li>' +
+                '<li><a data-edit="fontName Courier" style="font-family:'+"'Courier'" + '">Courier</a></li>' +
+                '<li><a data-edit="fontName Courier New" style="font-family:'+"'Courier New'" + '">Courier New</a></li>' +
+                '<li><a data-edit="fontName Comic Sans MS" style="font-family:'+"'Comic Sans MS'" + '">Comic Sans MS</a></li>' +
+                '<li><a data-edit="fontName Helvetica" style="font-family:'+"'Helvetica'" + '">Helvetica</a></li>' +
+                '<li><a data-edit="fontName Impact" style="font-family:'+"'Impact'" + '">Impact</a></li>' +
+                '<li><a data-edit="fontName Lucida Grande" style="font-family:'+"'Lucida Grande'" + '">Lucida Grande</a></li>' +
+                '<li><a data-edit="fontName Lucida Sans" style="font-family:'+"'Lucida Sans'" + '">Lucida Sans</a></li>' +
+                '<li><a data-edit="fontName Tahoma" style="font-family:'+"'Tahoma'" + '">Tahoma</a></li>' +
+                '<li><a data-edit="fontName Times" style="font-family:'+"'Times'" + '">Times</a></li>' +
+                '<li><a data-edit="fontName Times New Roman" style="font-family:'+"'Times New Roman'" + '">Times New Roman</a></li>' +
+                '<li><a data-edit="fontName Verdana" style="font-family:'+"'Verdana'" + '">Verdana</a></li></ul> ' +
+                '</div>' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" title="" data-original-title="Font Size"><i class="glyphicon glyphicon-text-height"></i>&nbsp;<b class="caret"></b></a>' +
+                '<ul class="dropdown-menu">' +
+                '<li><a data-edit="fontSize 5"><font size="5">Huge</font></a></li>' +
+                '<li><a data-edit="fontSize 3"><font size="3">Normal</font></a></li>' +
+                '<li><a data-edit="fontSize 1"><font size="1">Small</font></a></li>' +
+                '</ul>' +
+                '</div>' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary" data-edit="bold" title="" data-original-title="Bold (Ctrl/Cmd+B)"><i class="glyphicon glyphicon-bold"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="italic" title="" data-original-title="Italic (Ctrl/Cmd+I)"><i class="glyphicon glyphicon-italic"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="underline" title="" data-original-title="Underline (Ctrl/Cmd+U)"><i class="glyphicon glyphicon-text-width"></i></a>' +
+                '</div>' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary" data-edit="insertunorderedlist" title="" data-original-title="Bullet list"><i class="glyphicon glyphicon-list"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="insertorderedlist" title="" data-original-title="Number list"><i class="glyphicon glyphicon-list-alt"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="outdent" title="" data-original-title="Reduce indent (Shift+Tab)"><i class="glyphicon glyphicon-indent-left"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="indent" title="" data-original-title="Indent (Tab)"><i class="glyphicon glyphicon-indent-right"></i></a>' +
+                '</div>' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary" data-edit="justifyleft" title="" data-original-title="Align Left (Ctrl/Cmd+L)"><i class="glyphicon glyphicon-align-left"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="justifycenter" title="" data-original-title="Center (Ctrl/Cmd+E)"><i class="glyphicon glyphicon-align-center"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="justifyright" title="" data-original-title="Align Right (Ctrl/Cmd+R)"><i class="glyphicon glyphicon-align-right"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="justifyfull" title="" data-original-title="Justify (Ctrl/Cmd+J)"><i class="glyphicon glyphicon-align-justify"></i></a>' +
+                '</div>' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" title="" data-original-title="Hyperlink"><i class="glyphicon glyphicon-link"></i></a>' +
+                '<div class="dropdown-menu input-append">' +
+                '<input class="span2" placeholder="URL" type="text" data-edit="createLink">' +
+                '<button class="btn" type="button">Add</button>' +
+                '</div>' +
+                '<a class="btn btn-xs btn-primary" data-edit="unlink" title="" data-original-title="Remove Hyperlink"><i class="glyphicon glyphicon-remove"></i></a>' +
+                '</div>' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary" title="" id="pictureBtn" data-original-title="Insert picture (or just drag &amp; drop)"><i class="glyphicon glyphicon-picture"></i></a>' +
+                '<input type="file" data-role="magic-overlay" data-target="#pictureBtn" data-edit="insertImage" style="opacity: 0; position: absolute; top: 0px; left: 0px; width: 37px; height: 30px;">' +
+                '</div>' +
+                '<div class="btn-group">' +
+                '<a class="btn btn-xs btn-primary" data-edit="undo" title="" data-original-title="Undo (Ctrl/Cmd+Z)"><i class="glyphicon glyphicon-backward"></i></a>' +
+                '<a class="btn btn-xs btn-primary" data-edit="redo" title="" data-original-title="Redo (Ctrl/Cmd+Y)"><i class="glyphicon glyphicon-forward"></i></a>' +
+                '</div>' +
+                '</div>';
 
         }
     };
